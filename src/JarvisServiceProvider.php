dfe95080188ac90ed1233ba10f2f73b3886058e6
Jay Gohil <jay.gohil@techtic.com>
2019-12-23 18:56:03 +0530

    Updates

--- src/JarvisServiceProvider.php
+++ src/JarvisServiceProvider.php
@@ -10,9 +10,7 @@ class JarvisServiceProvider extends ServiceProvider {
     
     public function boot() {
         Schema::defaultStringLength('191');
-        // $this->loadRoutesFrom(__DIR__ . '/routes/web.php');
         $this->loadRoutesFrom(__DIR__ . '/routes/api.php');
-        // $this->loadViewsFrom(__DIR__ . '/resources/views', 'jarvisview');
     }
     
     public function register()
