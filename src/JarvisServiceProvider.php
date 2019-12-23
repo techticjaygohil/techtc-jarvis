62bbd1cff620e522113343d06e767677574eaefa
Jay Gohil <jay.gohil@techtic.com>
2019-12-23 19:30:39 +0530

    Updates

--- src/JarvisServiceProvider.php
+++ src/JarvisServiceProvider.php
@@ -7,59 +7,54 @@ use Illuminate\Support\Facades\Response;
 use Illuminate\Support\ServiceProvider;
 
 class JarvisServiceProvider extends ServiceProvider {
-    
+
     public function boot() {
-        Schema::defaultStringLength('191');
-        $this->loadRoutesFrom(__DIR__ . '/routes/api.php');
+        Schema::defaultStringLength( '191' );
+        $this->loadRoutesFrom( __DIR__ . '/routes/api.php' );
     }
-    
-    public function register()
-    {
+
+    public function register() {
         $this->registerPublishables();
     }
-    
-    public function registerPublishables()
-    {
-        $basePath = dirname(__DIR__);
+
+    public function registerPublishables() {
+        $basePath = dirname( __DIR__ );
 
         $arrayPublishable = [
             'migrations' => [
-                "$basePath/src/database/migrations" => database_path('migrations')
+                "$basePath/src/database/migrations" => database_path( 'migrations' )
             ],
             'seeds' => [
-                "$basePath/src/database/seeds" => database_path('seeds')
+                "$basePath/src/database/seeds" => database_path( 'seeds' )
             ],
             'controllers' => [
-                "$basePath/src/Http/Controllers" => app_path('Api/Controllers')
+                "$basePath/src/Http/Controllers" => app_path( 'Api/Controllers' )
             ],
             'requests' => [
-                "$basePath/src/Http/Requests" => app_path('Api/Requests')
+                "$basePath/src/Http/Requests" => app_path( 'Api/Requests' )
             ],
             'models' => [
-                "$basePath/src/models" => app_path('Models')
+                "$basePath/src/models" => app_path( 'Models' )
             ],
             'routes' => [
-                "$basePath/src/routes/api.php" => base_path('routes/api.php')
+                "$basePath/src/routes/api.php" => base_path( 'routes/api.php' )
             ],
             'notification' => [
-                "$basePath/src/Notifications/ForgetPasswordNotification.php" => app_path('Notifications/ForgetPasswordNotification.php')
-            ],
-            'ResponseMacroServiceProvider' => [
-                "$basePath/src/Providers/ResponseMacroServiceProvider.php" => app_path('Providers/ResponseMacroServiceProvider.php')
-            ],
-            'RouteServiceProvider' => [
-                "$basePath/src/Providers/RouteServiceProvider.php" => app_path('Providers/RouteServiceProvider.php')
+                "$basePath/src/Notifications/ForgetPasswordNotification.php" => app_path( 'Notifications/ForgetPasswordNotification.php' )
             ],
             'FileUploadTrait' => [
-                "$basePath/src/Traits/FileUploadTrait.php" => app_path('Traits/FileUploadTrait.php')
+                "$basePath/src/Traits/FileUploadTrait.php" => app_path( 'Traits/FileUploadTrait.php' )
             ],
             'configAuth' => [
-                "$basePath/src/config/auth.php" => config_path('auth.php')
-            ]
+                "$basePath/src/config/auth.php" => config_path( 'auth.php' )
+            ],
+            'ServiceProvider' => [
+                "$basePath/src/Providers" => app_path( 'Providers' )
+            ],
         ];
 
-        foreach ($arrayPublishable as $group => $paths) {
-            $this->publishes($paths, $group);
+        foreach ( $arrayPublishable as $group => $paths ) {
+            $this->publishes( $paths, $group );
         }
     }
 }
